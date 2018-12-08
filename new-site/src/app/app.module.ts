96111048aa65e9125438a948d4d3e01c5658e886
Susanta <susantasky@gmail.com>
2018-12-08 21:07:39 +0530

    components added

--- new-site/src/app/app.module.ts
+++ new-site/src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { CertificatesComponent } from './certificates/certificates.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AboutMeComponent,
+        CertificatesComponent,
+        ProjectsComponent,
+        ExperienceComponent,
+        NotFoundComponent
+    ],
+    imports: [
+        RouterModule,
+        BrowserModule,
+        AppRoutingModule,
+        RouterModule.forRoot([
+            {
+                path: '',
+                redirectTo: 'homepage',
+                pathMatch: 'full'
+            },
+            {
+                path: 'certificates',
+                component: CertificatesComponent
+            },
+            {
+                path: 'homepage',
+                component: AboutMeComponent
+            },
+            {
+                path: '**',
+                component: NotFoundComponent
+            }
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
