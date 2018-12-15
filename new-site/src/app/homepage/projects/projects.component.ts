9628f8e61788f406ac28423921670069d3fc873b
Susanta <susantasky@gmail.com>
2018-12-15 22:25:40 +0530

    projects rearranged

--- new-site/src/app/homepage/projects/projects.component.ts
+++ new-site/src/app/homepage/projects/projects.component.ts
@@ -9,19 +9,19 @@ export class ProjectsComponent implements OnInit {
 	projects = [
 		{
 			"id": 1,
-			"title": "Realtime Facial Recognition",
-			"intro": "Realtime Facial recognition system using Siamese neural network. The model uses face encodings for identifying users.",
-			"description": "Face Recognition system using Siamese Neural network. The model is based on the FaceNet model. OpenCV implementaion has been done for realtime face detection and recognition using the network. The model uses face encodings for identifying users.",
-			"url": "https://susantabiswas.github.io/FaceRecog/",
-			"img_url": "../../../assets/projects/face-recog/1.jpg" 
-		},
-		{
-			"id": 2,
 			"title": "Facial keypoint regression",
 			"intro": "Facial keypoints detection using 15 landmark points on human face",
 			"description": "Keypoint detection problem is a regression problem where we predict the coordinates for the landmarks on the face which are real numbers. Detection of facial keypoints is very useful for a number of tasks like facial recognition, detection etc. The keypoints selected in this code uses 15 keypoints representing the various coordinates on the human face.",
 			"url": "https://susantabiswas.github.io/facial-keypoint-regression/",
 			"img_url": "../../../assets/projects/regression-keypoint/1.jpg" 
+		},
+		{
+			"id": 2,
+			"title": "Realtime Facial Recognition",
+			"intro": "Realtime Facial recognition system using Siamese neural network. The model uses face encodings for identifying users.",
+			"description": "Face Recognition system using Siamese Neural network. The model is based on the FaceNet model. OpenCV implementaion has been done for realtime face detection and recognition using the network. The model uses face encodings for identifying users.",
+			"url": "https://susantabiswas.github.io/FaceRecog/",
+			"img_url": "../../../assets/projects/face-recog/1.jpg" 
 		},
 		{
 			"id": 3,
@@ -64,20 +64,20 @@ export class ProjectsComponent implements OnInit {
 			"img_url": "../../../assets/projects/facial-emotion/1.JPG"  
 		},
 		{
-			"id": 8,
-			"title": "Similar question finder",
-			"intro": "Find similar questions amongst a bunch of questions using word embeddings. Here Google's 300 dimensional word embeddings has been used.",
-			"description": "Find similar questions using word embeddings. Here we use Google's 300 dimensional word embeddings.",
-			"url": "https://susantabiswas.github.io/similar-question-finder/",
-			"img_url": "../../../assets/projects/similar-questions/1.JPG"  
+			"id": 12,
+			"title": "Voice Assistant",
+			"intro": "A RNN based voice application which can do tasks when it recognizes the user speaking the Trigger word. Here the trigger word is 'activate'.",
+			"description": "A voice based assistant application which executes the assigned command on detecting the trigger word from the user voice. A trigger word is a word to which the model gets triggered once it detects it. Like for example 'Hey Cortana' or 'Ok Google'. Here the trigger word used is 'activate'. The Model used for detecting trigger word uses a deep Recurrent Neural Network with Gated Recurrent Units (GRU).",
+			"url": "https://susantabiswas.github.io/TriggerWordAssistant/",
+			"img_url": "../../../assets/projects/trigger-word/1.jpg"
 		},
 		{
 			"id": 9,
-			"title": "Name generation using RNN",
-			"intro": "Indian names generation using Gated Recurrent units. It uses a character level language model for this task. This model was trained for generating indian names.",
-			"description": "Indian name generation using Gated Recurrent units. It uses a character level language model for this task.",
-			"url": "https://susantabiswas.github.io/Name-Gen-RNN/",
-			"img_url": "../../../assets/projects/name-gen/1.JPG"  
+			"title": "Web based facial Authentication system",
+			"intro": "This is a web based facial log in/ Sign up website which uses Facial recognition for user authentication. It uses flask server for running the keras neural network model. The model runs on the server side and accepts requests from clients. Facial recognition is based on Facenet model.",
+			"description": "This is a web based facial log in/ Sign up website which uses Facial recognition for user authentication. It uses flask server for running the keras neural network model. The model runs on the server side and accepts requests from clients. Facial recognition is based on Facenet model.",
+			"url": "https://susantabiswas.github.io/facial-login-web/",
+			"img_url": "../../../assets/projects/facial-login-web/1.jpg" 
 		},
 		{
 			"id": 10,
@@ -89,19 +89,19 @@ export class ProjectsComponent implements OnInit {
 		},
 		{
 			"id": 11,
-			"title": "Web based facial Authentication system",
-			"intro": "This is a web based facial log in/ Sign up website which uses Facial recognition for user authentication. It uses flask server for running the keras neural network model. The model runs on the server side and accepts requests from clients. Facial recognition is based on Facenet model.",
-			"description": "This is a web based facial log in/ Sign up website which uses Facial recognition for user authentication. It uses flask server for running the keras neural network model. The model runs on the server side and accepts requests from clients. Facial recognition is based on Facenet model.",
-			"url": "https://susantabiswas.github.io/facial-login-web/",
-			"img_url": "../../../assets/projects/facial-login-web/1.jpg" 
+			"title": "English to French Translation using NMT",
+			"intro": "Sequence to sequence model for Language translation from English to French. This model uses an Encoder-Decoder with LSTM cells.",
+			"description": "Sequence to sequence model for Language translation from English to French. This model uses an Encoder-Decoder with LSTM cells.",
+			"url": "https://susantabiswas.github.io/Neural-Language-Translator/",
+			"img_url": "../../../assets/projects/language-translator/1.JPG" 
 		},
 		{
 			"id": 12,
-			"title": "Neural Date translator",
-			"intro": "Date translation from conventional Human readable format to machine readable format(YYYY-MM-DD) using Neural Machine Translation. The task is to convert dates to a more normalized format like YYYY-MM-DD.",
-			"description": "Date translation from conventional Human readable format to machine readable format(YYYY-MM-DD) using Neural Machine Translation. For example a date in human readable format can be : 'sunday 15 september 2013', '29-oct-1997' or '30 august 1985' etc. The task is to convert this to a more normalized format like YYYY-MM-DD.",
-			"url": "https://susantabiswas.github.io/Neural-Date-Translator/",
-			"img_url": "../../../assets/projects/date-translator/1.JPG"
+			"title": "Similar question finder",
+			"intro": "Find similar questions amongst a bunch of questions using word embeddings. Here Google's 300 dimensional word embeddings has been used.",
+			"description": "Find similar questions using word embeddings. Here we use Google's 300 dimensional word embeddings.",
+			"url": "https://susantabiswas.github.io/similar-question-finder/",
+			"img_url": "../../../assets/projects/similar-questions/1.JPG"  
 		},
 		{
 			"id": 13,
@@ -113,35 +113,35 @@ export class ProjectsComponent implements OnInit {
 		},
 		{
 			"id": 14,
-			"title": "English to French Translation using NMT",
-			"intro": "Sequence to sequence model for Language translation from English to French. This model uses an Encoder-Decoder with LSTM cells.",
-			"description": "Sequence to sequence model for Language translation from English to French. This model uses an Encoder-Decoder with LSTM cells.",
-			"url": "https://susantabiswas.github.io/Neural-Language-Translator/",
-			"img_url": "../../../assets/projects/language-translator/1.JPG" 
+			"title": "Name generation using RNN",
+			"intro": "Indian names generation using Gated Recurrent units. It uses a character level language model for this task. This model was trained for generating indian names.",
+			"description": "Indian name generation using Gated Recurrent units. It uses a character level language model for this task.",
+			"url": "https://susantabiswas.github.io/Name-Gen-RNN/",
+			"img_url": "../../../assets/projects/name-gen/1.JPG"  
 		},
 		{
 			"id": 15,
-			"title": "Word Analogy",
-			"intro": "Finding word analogies using GLoVe word embeddings. In the word analogy task, we have 'a is to b as c is to __'. For example is 'boy is to girl as king is to queen' .",
-			"description": "Finding word analogies using GLoVe word embeddings. In the word analogy task, we have 'a is to b as c is to __'. For example is 'boy is to girl as king is to queen' .",
-			"url": "https://susantabiswas.github.io/Word-Analogy/",
-			"img_url": "../../../assets/projects/word-analogy/1.JPG"
-		},
-		{
-			"id": 16,
-			"title": "Voice Assistant",
-			"intro": "A RNN based voice application which can do tasks when it recognizes the user speaking the Trigger word. Here the trigger word is 'activate'.",
-			"description": "A voice based assistant application which executes the assigned command on detecting the trigger word from the user voice. A trigger word is a word to which the model gets triggered once it detects it. Like for example 'Hey Cortana' or 'Ok Google'. Here the trigger word used is 'activate'. The Model used for detecting trigger word uses a deep Recurrent Neural Network with Gated Recurrent Units (GRU).",
-			"url": "https://susantabiswas.github.io/TriggerWordAssistant/",
-			"img_url": "../../../assets/projects/trigger-word/1.jpg"
-		},
-		{
-			"id": 17,
 			"title": "Convert phrase to emoji",
 			"intro": "Deep RNN for expressing english phrases with emoji. Using a deep LSTM network the model associates an english input sentence with an emoji.",
 			"description": "Express your words with the power of Emoji. Using a deep LSTM network the model associates an english input sentence with an emoji.",
 			"url": "https://susantabiswas.github.io/DeepEmoji/",
 			"img_url": "../../../assets/projects/phrase-emoji/1.JPG"
+		},
+		{
+			"id": 16,
+			"title": "Neural Date translator",
+			"intro": "Date translation from conventional Human readable format to machine readable format(YYYY-MM-DD) using Neural Machine Translation. The task is to convert dates to a more normalized format like YYYY-MM-DD.",
+			"description": "Date translation from conventional Human readable format to machine readable format(YYYY-MM-DD) using Neural Machine Translation. For example a date in human readable format can be : 'sunday 15 september 2013', '29-oct-1997' or '30 august 1985' etc. The task is to convert this to a more normalized format like YYYY-MM-DD.",
+			"url": "https://susantabiswas.github.io/Neural-Date-Translator/",
+			"img_url": "../../../assets/projects/date-translator/1.JPG"
+		},
+		{
+			"id": 17,
+			"title": "Word Analogy",
+			"intro": "Finding word analogies using GLoVe word embeddings. In the word analogy task, we have 'a is to b as c is to __'. For example is 'boy is to girl as king is to queen' .",
+			"description": "Finding word analogies using GLoVe word embeddings. In the word analogy task, we have 'a is to b as c is to __'. For example is 'boy is to girl as king is to queen' .",
+			"url": "https://susantabiswas.github.io/Word-Analogy/",
+			"img_url": "../../../assets/projects/word-analogy/1.JPG"
 		},
 		{
 			"id": 18,
