c0d3b75a95b81b8f5ad5c9f8d8371d143ddd4b77
Susanta <susantasky@gmail.com>
2018-12-15 21:55:20 +0530

    minor changes

--- new-site/src/app/certificates/certificates.component.ts
+++ new-site/src/app/certificates/certificates.component.ts
@@ -10,6 +10,7 @@ export class CertificatesComponent implements OnInit {
 	constructor() { }
 
 	ngOnInit() {
+		// scroll to the top of the page when the component loads
 		window.scroll(0, 0);
 	}
 
